d88491bf46e87c214b2d21ba4987982d3009d1c8
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-07 21:43:27 +0530

    ADDED: program to find count of persons detail present in contact book

--- AddressBookJS.js
+++ AddressBookJS.js
@@ -160,3 +160,9 @@ addressBookArray.push(contact4)
 let param3 = prompt("Enter the First Name (contact to delete):  ")
 let param4 = prompt("Enter the Last Name (contact to delete):  ")
 deleteContact(param3, param4)
+
+function count(counter)
+{
+    return counter + 1;
+}
+console.log("Total Number of Contacts: " + addressBookArray.reduce(count, 0))
